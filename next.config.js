0b42bf5f9bfadb06782690af9b2a4db24d490e93
wndeepak31 <dnishad031@gmail.com>
2025-12-15 12:54:24 +0530

    allow iframe embedding for shopify

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
